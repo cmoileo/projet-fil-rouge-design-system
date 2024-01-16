98a6d6b08851749683892431b79e49819ab0253b
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-01-16 19:45:53 +0100

    fix rollup config

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,8 @@
-declare const Button = (props) => {
-    return (React.createElement("button", null, props.label));
-};
-//# sourceMappingURL=index.d.ts.map
+import React from 'react';
+
+interface ButtonProps {
+    label: string;
+}
+declare const Button: (props: ButtonProps) => React.JSX.Element;
 
 export { Button };
